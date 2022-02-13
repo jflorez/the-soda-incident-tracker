74b29e643290d88fe5c2c5abd9fe48f7f33857db
Juan Florez <jflorez@planittesting.com>
2022-02-13 19:11:26 +1100

    MVP completed

--- bot/bot_server.js
+++ bot/bot_server.js
@@ -1,28 +1,36 @@
 require('dotenv').config();
+const { insertIncident, getLatestIncident } = require('../db/db_connector');
 const { Client, Intents } = require('discord.js');
 const token = process.env.DISCORD_BOT_TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const bots = {};
-bots['!track'] = function(message, arg) {
-	message.channel.send(`I'm tracking ${message.member.displayName} for ${arg} incidents`);
+bots['!track'] = async function(message, arg) {
+	await insertIncident(message.member.displayName, arg);
+	message.channel.send(`${message.member.displayName} you have created a tracker for ${arg}`);
 };
-bots['!oops'] = function(message, arg) {
-	message.channel.send(`So you (${message.member.displayName}) caved and had a soda? back to zero`);
+bots['!oops'] = async function(message, arg) {
+	const {user, item, oops} = await getLatestIncident(message.member.displayName);
+	await insertIncident(message.member.displayName, item);
+	message.channel.send(`${message.member.displayName} your ${item} tracker is now back to zero`);
 };
-bots['!incident'] = function(message, arg) {
-	message.channel.send(`You (${message.member.displayName}) have not had a soda in this many days`);
+bots['!incidents'] = async function(message, arg) {
+	const {user, item, oops} = await getLatestIncident(message.member.displayName);
+	const oopsDate = new Date(oops);
+	const now = new Date();
+	const days = Math.floor((now - oopsDate)/(1000 * 60 * 60 * 24));
+	message.channel.send(`${message.member.displayName} is has been ${days} days since your last oops!`);
 };
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message =>  {
 	const {content} = message;
 	const [command, arg='soda'] = content.split(' ', 2);
 	if(command in bots) {
-		bots[command](message, arg);
+		await bots[command](message, arg);
 	}
 });
 
---
+++ db/db_connector.js
@@ -0,0 +1,36 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'sodatracker',
+    connectionLimit: 5
+});
+module.exports.insertIncident = async (user, item) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const res = await conn.query("INSERT INTO tracker (user,item) values (?,?)", [user, item]);
+        console.log(res);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
+}
+
+module.exports.getLatestIncident = async (user) => {
+    let conn;
+    let rows=[];
+    try {
+        conn = await pool.getConnection();
+        rows = await conn.query("SELECT user, item, oops FROM tracker WHERE user=? ORDER BY oops DESC LIMIT 1", [user]);
+        console.log(rows[0]);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) await conn.end();
+        const ret = {user: rows[0]['user'], item: rows[0]['item'], oops: rows[0]['oops']};
+        return ret;
+    }
+}
